ff9012277ac745d94a8993f5b896684568911049
hicham-oualla <157011639+hicham-oualla@users.noreply.github.com>
2024-03-31 23:03:54 +0000

    modifier la function rechercher

--- javas.js
+++ javas.js
@@ -1,41 +1,40 @@
 async function get() {
-    // fetich kyjib les contact mn api
+    // // fetich kyjib les contact mn api
     const response = await fetch("https://randomuser.me/api/");
-//    chnager les ifo from api to json file
+    // Converting API to JSON
     const data = await response.json();
-     console.log(data);
-//les cartes
-    const listcart=document.querySelector(".cartes");
-    // results= array f api
-data.results.forEach(user => {
-// creat div with classe="col-md-4" in list cart
-    const cart=document.createElement("div");
-    cart.classList.add("col-md-4");
-//    pour boucler les cart aymchi lcart ou ghyht fiha dkchi li jb mn api
-    cart.innerHTML=`    
-    <div class="card text-center card-color-1">
-        <img src="${user.picture.large}" class="card-img-top profile-img mx-auto mt-3" alt="Image de profil">
-        <div class="card-body">
-            <h5 class="card-title">${user.name.last} ${user.name.first}</h5>
-            <p class="card-text">Numéro de téléphone : ${user.phone}</p>
-            <p class="card-text">E-mail : ${user.email}</p>
-        </div>
-    </div>
-</div> `;
+    // Targeting the container for cards
+    const cardContainer = document.querySelector(".cartes");
+    // Looping through the results from API
+    data.results.forEach(user => {
+        // creat div with classe="col-md-4" in list cart
+        const card = document.createElement("div");
+        card.classList.add("col-md-4");
 
+        card.innerHTML = `
+            <div class="card text-center card-color-1">
+                <img src="${user.picture.large}" class="card-img-top profile-img mx-auto mt-3" alt="Image de profil">
+                <div class="card-body">
+                    <h5 class="card-title">${user.name.last} ${user.name.first}</h5>
+                    <p class="card-text">Numéro de téléphone : ${user.phone}</p>
+                    <p class="card-text">E-mail : ${user.email}</p>
+                </div>
+            </div>
+        `;
 
-    listcart.appendChild(cart);
-});
-
+        cardContainer.appendChild(card);
+    });
 }
-function boucler(){
-    var i;
-    for (i = 0; i < 12; i++) {
+
+function afficher() {
+    
+    for (let i = 0; i < 12; i++) {
         get();
     }
 }
 
-boucler();
+afficher();
+
 function searchContact() {
     const searchValue = document.getElementById('searchInput').value.toLowerCase();
     const cards = document.querySelectorAll('.card');
@@ -43,12 +42,9 @@ function searchContact() {
     cards.forEach(card => {
         const cardName = card.querySelector('.card-title').innerText.toLowerCase();
         if (cardName.includes(searchValue)) {
-            card.classList.remove('d-none'); // Show the card
+            card.classList.remove('d-none'); 
         } else {
-            card.classList.add('d-none'); // Hide the card
+            card.classList.add('d-none'); 
         }
     });
 }
-
-
-
